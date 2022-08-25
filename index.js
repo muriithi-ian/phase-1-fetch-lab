a95753dbb6b33cf3dc7a7dfee800321a22624a12
Muriithi Ian <imuriithiian@gmail.com>
2022-08-25 15:30:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+const fetch = require("node-fetch");
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+	// To pass the tests, don't forget to return your fetch!
+	return fetch("https://anapioficeandfire.com/api/books")
+		.then((res) => res.json())
+		.then((res) => {
+			renderBooks(res);
+		});
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+	const main = document.querySelector("main");
+	books.forEach((book) => {
+		const h2 = document.createElement("h2");
+		h2.innerHTML = book.name;
+		main.appendChild(h2);
+	});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {
+	fetchBooks();
 });
